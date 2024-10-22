10cd994e9818a5199d0da6f9bdb2691e5ede1800
Redheart <battlefield980@mail.ru>
2024-10-22 16:36:44 +0500

    Task10
    Малыгин Никита. Задача 10.

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -1,5 +1,8 @@
 package com.example.task10;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
@@ -7,8 +10,27 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
+
+        boolean result = FALSE;
+        if((int)a == (int)b){
+            String fractionalA = Float.toString(a);
+            String fractionalB = Float.toString(b);
+            fractionalA = fractionalA.substring(fractionalA.lastIndexOf(".") + 1);
+            fractionalB = fractionalB.substring(fractionalB.lastIndexOf(".") + 1);
+            if (fractionalA.length() > precision)
+                fractionalA = fractionalA.substring(0, precision);
+            else
+                while (fractionalA.length() < precision)
+                    fractionalA += "0";
+            if (fractionalB.length() > precision)
+                fractionalB = fractionalB.substring(0, precision);
+            else
+                while (fractionalB.length() < precision)
+                    fractionalB += "0";
+            if(fractionalA.equals(fractionalB))
+                result = TRUE;
+        }
+        return result;
 
     }
 
@@ -20,7 +42,6 @@ public class Task10 {
 
         boolean result = compare(sum, c, 2);
         System.out.println(result);
-
     }
 
 }
