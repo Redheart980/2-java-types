f63877fbe084a084d45ea980bf3df3041987d161
Redheart <battlefield980@mail.ru>
2024-10-22 13:09:41 +0500

    Task04
    Малыгин Никита. Задание 04

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -3,10 +3,26 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        float output;
+        switch (operation)
+        {
+            case "+":
+                output = a+b;
+                break;
+            case "-":
+                output = a-b;
+                break;
+            case "/":
+                output = (float) a/b;
+                break;
+            case "*":
+                output = a*b;
+                break;
+            default:
+                output = 0;
+                break;
+        }
+        return output;
     }
 
     public static void main(String[] args) {
