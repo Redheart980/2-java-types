0824b24c31a702e7c9b1d89093a590d4de1ee93d
Redheart <battlefield980@mail.ru>
2024-10-22 13:32:42 +0500

    Task06
    Малыгин Никита. Задание 06.

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,10 +3,13 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int num = Math.abs(x + y);
+        int count = 1;
+        while (num >= 10){
+            count++;
+            num /= 10;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
